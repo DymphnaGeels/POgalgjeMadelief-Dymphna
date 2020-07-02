5b4bf04d2cb8fb72eab723d15bdf975d014e31a9
Dymphna Geels <110533@st-maartenscollege.nl>
2020-07-02 11:55:11 +0000

    while loop als invoer 1 letter is.

--- main.py
+++ main.py
@@ -20,3 +20,24 @@ def play(word):
    print(word_completion)
    print("\n")
    print('het woord heeft', len(word), 'letters')
+
+   while not guessed and tries > 0:
+       guess = input("Typ een letter of een woord:").upper()
+       if len(guess) == 1 and guess.isalpha():
+           if guess in guessed_letters:
+               print("Deze letter heb je al geraden.", guess)
+           elif guess not in word:
+               print(guess, "zit niet in het woord.")
+               tries -= 1
+               guessed_letters.append(guess)
+           else:
+               print("Goed bezig,", guess, "zit in het woord!")
+               guessed_letters.append(guess)
+               word_as_list = list(word_completion)
+               indices = [i for i, letter in enumerate(word) if letter == guess]
+               for index in indices:
+                   word_as_list[index] = guess
+               word_completion = "".join(word_as_list)
+               if "_" not in word_completion:
+                   guessed = True
+
