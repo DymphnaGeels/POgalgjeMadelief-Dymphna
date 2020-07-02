aa52c01ecf72c20196d7e1692f45d934809fbc2d
Dymphna Geels <110533@st-maartenscollege.nl>
2020-07-02 11:51:00 +0000

    define verschillende onderdelen. En list toegevoegd.

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ from words import word_list
 def get_word():
    word = random.choice(word_list)
    return word.upper()
+
+def play(word):
+   word_completion = "_" * len(word)
+   guessed = False
+   guessed_letters = []
+   guessed_words = []
+   tries = 5
+   print("Laten we galgje spelen!")
+   print('De regels:')
+   list = ['-na een ingevoerde letter op enter drukken.', '-na vijf foute antwoorden ben je af.']
+   for item in list:
+     print(item)
+   print(display_hangman(tries))
+   print(word_completion)
+   print("\n")
